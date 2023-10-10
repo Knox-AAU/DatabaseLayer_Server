41ff398125ed86f6542c4eed3d6d22b5ff77dcb4
Mcklmo <87199710+Mcklmo@users.noreply.github.com>
2023-10-10 11:22:42 +0200

    [update] integrate database in endpoint

--- pkg/graph/service.go
+++ pkg/graph/service.go
@@ -2,20 +2,20 @@ package graph
 
 // Service provides operations
 type Service interface {
-	FindAll() (*Node, error)
-	Find(string) (*Node, error)
+	FindAll() (*[]Triple, error)
+	Find(string) (*[]Triple, error)
 	Delete(string) error
-	Update(*Node) error
-	Store(*Node) error
+	Update(*Triple) error
+	Create(*Triple) error
 }
 
 // Repository sends queries and turns response from virtuoso server into a Node with eventual children
 type Repository interface {
-	FindAll() (*Node, error)
-	Find(string) (*Node, error)
+	FindAll() (*[]Triple, error)
+	Find(string) (*[]Triple, error)
 	Delete(string) error
-	Update(*Node) error
-	Store(*Node) error
+	Update(*Triple) error
+	Create(*Triple) error
 }
 
 // service implements Service interface
@@ -28,11 +28,11 @@ func NewService(r Repository) Service {
 	return &service{r}
 }
 
-func (s *service) FindAll() (*Node, error) {
+func (s *service) FindAll() (*[]Triple, error) {
 	return s.r.FindAll()
 }
 
-func (s *service) Find(query string) (*Node, error) {
+func (s *service) Find(query string) (*[]Triple, error) {
 	return s.r.Find(query)
 }
 
@@ -40,50 +40,12 @@ func (s *service) Delete(query string) error {
 	return s.r.Delete(query)
 }
 
-func (s *service) Update(n *Node) error {
+func (s *service) Update(n *Triple) error {
 	return s.r.Update(n)
 }
 
-func (s *service) Store(n *Node) error {
-	return s.r.Store(n)
-}
-
-func NewGetAllQuery() string {
-	return `SELECT ?s ?p ?o
-	WHERE {
-		?s ?p ?o
-	}
-	LIMIT 100`
-}
-
-// VirtuosoToNode takes a virtuoso object and returns a Node
-func VirtuosoToNode(v *VirtuosoObject) *[]Node {
-	result := []Node{}
-
-	//for _, b := range v.Results.Bindings {
-
-	/*
-		bindingExists = false
-	*/
-
-	//for _, node := range result {
-
-	/*
-		if (binding.subject.value = node.value)
-			node.children.append(call helperfunction getNodeFromURI with b.object.value)
-			bindingExists = true
-			break
-	*/
-
-	//}
-
-	/*
-		if (bindingExists = false)
-			result.append(call helperfunction getNodeFromURI with b.subject.value)
-	*/
-	//}
-
-	return &result
+func (s *service) Create(n *Triple) error {
+	return s.r.Create(n)
 }
 
 // getNodeFromURI will recieve bindingattribute and toggle URI and literal, if URI will fetch nodes, if literal returns node without fetch
--- pkg/storage/virtuoso/http/repository.go
+++ pkg/storage/virtuoso/http/repository.go
@@ -1,6 +1,14 @@
 package http
 
-import "github.com/Knox-AAU/DatabaseLayer_Server/pkg/graph"
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/url"
+
+	"github.com/Knox-AAU/DatabaseLayer_Server/pkg/graph"
+	"github.com/Knox-AAU/DatabaseLayer_Server/pkg/storage"
+)
 
 type virtuosoRepository struct {
 	VirtuosoServerURL string
@@ -12,22 +20,52 @@ func NewVirtuosoRepository(url string) graph.Repository {
 	}
 }
 
-func (r virtuosoRepository) FindAll() (*graph.Node, error) {
-	return &graph.Node{}, nil
+func (r virtuosoRepository) FindAll() (*[]graph.Triple, error) {
+	res, err := http.Get(r.VirtuosoServerURL + "?" + fomatQuery(storage.GetAll))
+	if err != nil {
+		return nil, err
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(res.Body)
+
+	virtuosoRes := graph.VirtuosoResponse{}
+
+	err = json.Unmarshal(buf.Bytes(), &virtuosoRes)
+	if err != nil {
+		return nil, err
+	}
+
+	return virtuosoResponseToTriples(virtuosoRes), nil
 }
 
-func (r virtuosoRepository) Find(serviceQuery string) (*graph.Node, error) {
-	return &graph.Node{}, nil
+func (r virtuosoRepository) Find(serviceQuery string) (*[]graph.Triple, error) {
+	return &[]graph.Triple{}, nil
 }
 
 func (r virtuosoRepository) Delete(serviceQuery string) error {
 	return nil
 }
 
-func (r virtuosoRepository) Update(node *graph.Node) error {
+func (r virtuosoRepository) Update(node *graph.Triple) error {
 	return nil
 }
 
-func (r virtuosoRepository) Store(node *graph.Node) error {
+func (r virtuosoRepository) Create(node *graph.Triple) error {
 	return nil
+}
+
+// formatQuery adds necessary parameters for virtuoso
+func fomatQuery(query string) string {
+	params := url.Values{}
+	params.Add("query", query)
+	params.Add("format", "json")
+	return params.Encode()
+}
+
+func virtuosoResponseToTriples(response graph.VirtuosoResponse) *[]graph.Triple {
+	result := make([]graph.Triple, len(response.Results.Bindings))
+
+	copy(result, response.Results.Bindings)
+
+	return &result
 }
