2c09df27e93ad1eb0633031cbc843d85509f4cd1
Matthias Jakobsen <mattja21@student.aau.dk>
2023-10-24 12:43:10 +0200

    [Update] Improved filtering

--- pkg/storage/sparql/queries.go
+++ pkg/storage/sparql/queries.go
@@ -9,22 +9,27 @@ import (
 var GetAll = fmt.Sprintf(`SELECT ?%s ?%s ?%s WHERE { ?%s ?%s ?%s }`,
 	graph.Subject, graph.Predicate, graph.Object, graph.Subject, graph.Predicate, graph.Object)
 
-func Builder(nodes, edges []string, depth int) string {
-	if len(nodes) == 0 && len(edges) == 0 {
+func Builder(edges, subjects, objects []string, depth int) string {
+	if len(subjects) == 0 && len(objects) == 0 && len(edges) == 0 {
 		return GetAll
 	}
 
 	result := fmt.Sprintf(`SELECT ?%s ?%s ?%s WHERE { ?%s ?%s ?%s . FILTER (`,
 		graph.Subject, graph.Predicate, graph.Object, graph.Subject, graph.Predicate, graph.Object)
 
-	if len(nodes) > 0 {
-		result += buildSubQuery(nodes, graph.Subject)
-		result += " && "
-		result += buildSubQuery(nodes, graph.Object)
+	if len(subjects) > 0 {
+		result += buildSubQuery(subjects, graph.Subject)
+	}
+
+	if len(objects) > 0 {
+		if len(subjects) > 0 {
+			result += " && "
+		}
+		result += buildSubQuery(objects, graph.Object)
 	}
 
 	if len(edges) > 0 {
-		if len(nodes) > 0 {
+		if len(subjects) > 0 || len(objects) > 0 {
 			result += " && "
 		}
 		result += buildSubQuery(edges, graph.Predicate)
